9fba967ec7c5c147d821d6da74f8549fc67067f2
Sheepex <u0224079@nkfust.edu.tw>
2015-06-09 20:23:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', function($scope) {
-  $scope.name = 'World';
+  
+  $scope.items = ['第一題', '第二題', '第三題', '第四題', '第五題'];
+  $scope.selection = $scope.items[0];
+  
+  $scope.change1 = function(){
+$scope.selection = ["第一題"];
+  }
+  $scope.change2 = function(){
+$scope.selection = ["第二題"];
+  }
+  $scope.change3 = function(){
+$scope.selection = ["第三題"];
+  }
+  $scope.change4 = function(){
+$scope.selection = ["第四題"];
+  }
+  $scope.change5 = function(){
+$scope.selection = ["第五題"];
+  }
+  $scope.addfontsize = function(){
+     $scope.x =$scope.x + 1;
+  }
+  $scope.redfontsize = function(){
+    if ($scope.x > 0){
+     $scope.x =$scope.x - 1;}
+  }
+  $scope.refontsize = function(){
+     $scope.x = 14;
+  }
+
 });
 
 
